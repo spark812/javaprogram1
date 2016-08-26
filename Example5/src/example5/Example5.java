e608135f7add362dc382165c176eb123dc18a924
Sarah Park <sarahpark@Sarahs-MacBook-Air.local>
2016-08-25 22:34:03 -0700

    learning to upload

---
+++ Example5/src/example5/Example5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example5;
+
+/**
+ *
+ * @author sarahpark
+ */
+public class Example5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int w = 10;
+        double x = 10.0;
+        
+        x = x /3;
+        System.out.print(x);
+        
+    }
+    
+}
